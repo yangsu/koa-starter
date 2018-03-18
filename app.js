f61d0ebbe701fba8bad15f7ad50a9e383afebc9c
Yang Su <yang@quip.com>
2018-03-18 12:44:29 -0700

    remove useless middleware

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ app
     })
   )
   .use(router.routes())
-  .use(router.allowedMethods())
-  .use(ctx => {
-    ctx.status = 404;
-    ctx.body = "Not Found";
-  });
+  .use(router.allowedMethods());
 
 module.exports = app;
